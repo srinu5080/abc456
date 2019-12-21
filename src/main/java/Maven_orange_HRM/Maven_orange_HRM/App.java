e3e371b225f68b1a0bab01119c08d3fe83b7a521
Siva Reddy <Siva Reddy@192.168.1.18>
2019-12-21 10:55:04 +0530

    first234[21/12/2019]

---
+++ src/main/java/Maven_orange_HRM/Maven_orange_HRM/App.java
@@ -0,0 +1,13 @@
+package Maven_orange_HRM.Maven_orange_HRM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Maven_orange_HRM/Maven_orange_HRM/Sampleprg.java
@@ -0,0 +1,5 @@
+package Maven_orange_HRM.Maven_orange_HRM;
+
+public class Sampleprg {
+
+}
