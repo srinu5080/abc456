a2b7abcb5783800431b9afb254c7acb0fbe638a3
Siva Reddy <Siva Reddy@192.168.1.18>
2019-12-21 10:57:18 +0530

    fouth3[21/12]

--- src/main/java/Maven_orange_HRM/Maven_orange_HRM/Sampleprg.java
+++ src/main/java/Maven_orange_HRM/Maven_orange_HRM/Sampleprg.java
@@ -1,5 +1,16 @@
 package Maven_orange_HRM.Maven_orange_HRM;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Sampleprg {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","E:\\srinuuuu data\\Java&Selenium\\Driver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.get("http://testingmasters.com/hrm");
+	}
 
 }
